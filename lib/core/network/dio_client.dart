48bff0aa5ff4f8c3d8d6f06880e2684cad474136
seongjinYU <128691265+seongjinYU@users.noreply.github.com>
2025-11-03 00:47:11 +0900

    로그인 버튼 위치 수정, fcm 관련 로그, try,catch 반영

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -5,8 +5,8 @@ import 'package:dio/dio.dart';
 class DioClient {
   final Dio _dio = Dio(BaseOptions(
 
-    // baseUrl: 'https://ssuchaehwa.duckdns.org',
-    baseUrl: 'http://192.168.123.123:8080',
+    baseUrl: 'https://ssuchaehwa.duckdns.org',
+    // baseUrl: 'http://192.168.123.106:8080', // 개발용
     connectTimeout: const Duration(seconds: 30),
     receiveTimeout: const Duration(seconds: 30),
     contentType: 'application/json',
